92b2572a6eec4c544aca27b78998580fd2e97dcd
srv27 <srv2795@gmail.com>
2022-02-13 20:22:55 +0530

    Print an array in java

---
+++ demo/src/main/java/com/example/array/ArrayDemo1.java
@@ -0,0 +1,15 @@
+package com.example.array;
+
+import java.util.Scanner;
+
+public class ArrayDemo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for(int i: arr)
+            arr[i] = sc.nextInt();
+
+        
+    }
+}
