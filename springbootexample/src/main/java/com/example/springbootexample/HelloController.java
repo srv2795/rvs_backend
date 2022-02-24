5a7bd1521710e8677ec7400308f614f4213427bc
srv27 <srv2795@gmail.com>
2022-02-24 23:29:56 +0530

    new example spring boot application.

---
+++ springbootexample/src/main/java/com/example/springbootexample/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.springbootexample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseBody
+@RequestMapping("/")
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello Spring boot....";
+    }
+
+    @GetMapping("/sms")
+    public String GetMessage() {
+        return "I am new to this technology....";
+    }
+}
