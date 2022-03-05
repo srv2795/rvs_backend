770fa55fa74a892957ef27f4da6a865303f14cff
srv27 <srv2795@gmail.com>
2022-03-05 22:18:56 +0530

    implementation of hytrix fault tolerance lirary in microservices application

---
+++ microservicesdemo/hystrixApp/src/test/java/com/example/hystrixApp/HystrixAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hystrixApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
