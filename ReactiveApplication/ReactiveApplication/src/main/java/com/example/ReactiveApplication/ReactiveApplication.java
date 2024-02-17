3e545eede9da7ea44508ef49082d085a5e637692
Ran Vijay Singh <99132985+srv2795@users.noreply.github.com>
2024-02-17 21:11:08 +0530

    trying to fix timezone error in sql database connection

--- ReactiveApplication/ReactiveApplication/src/main/java/com/example/ReactiveApplication/ReactiveApplication.java
+++ ReactiveApplication/ReactiveApplication/src/main/java/com/example/ReactiveApplication/ReactiveApplication.java
@@ -2,15 +2,19 @@ package com.example.ReactiveApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.r2dbc.ConnectionFactoryOptionsBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 import java.util.Locale;
 import java.util.TimeZone;
 
+@EnableWebFlux
+@EnableR2dbcRepositories
 @SpringBootApplication
 public class ReactiveApplication {
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.getDefault());
-		TimeZone.setDefault(TimeZone.getTimeZone("IST"));
 		SpringApplication.run(ReactiveApplication.class, args);
 	}
 }
